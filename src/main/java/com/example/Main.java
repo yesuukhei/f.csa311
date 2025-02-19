84962ee0c5cf8c98043a951e4a28c3cec8efbb68
Yesuukehi <yesuu1028@gmail.com>
2025-02-19 17:09:16 +0800

    my change

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Yesuukhei!");
+        System.out.println("yu bnda!");
+        System.out.println("bro!");
     }
 }
